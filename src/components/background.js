746fb692df05b327ab0356bb2c69ccfbfd78ed71
aditya2409 <adityajainakj123@gmail.com>
2022-12-16 16:17:46 +0530

    background problem solved

--- src/components/background.js
+++ src/components/background.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import acss from './css_folder/bcss.css';
-export default function 
-() {
+export default function Background() {
   return (
     <div class="lines" style={acss}>
     <div class="line"></div>
